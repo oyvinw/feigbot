e009762af44d853f0ee6ac99210cfad3079cdea5
oyvinw <p0l4re@gmail.com>
2023-04-07 15:25:59 +0200

    refactored code to make it a lot shorter and easier to read, also added language support to most commands

--- feigbot.py
+++ feigbot.py
@@ -11,6 +11,11 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 db = TinyDB('db.json')
 
+class MatchData:
+    def __init__(self, match, steam_id: int):
+        self.match = match
+        self.steam_id = steam_id
+
 def get_steam_id(discord_user):
     Q = Query()
     return db.search(Q.discord_user == f'{discord_user}')[0]['steam_user_id_32']
@@ -20,18 +25,29 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.reply("Jeg har ikke tillit til deg")
 
-@bot.command(name="perf", help="Use this command to see how you did last game")
-async def flink_sjekk(ctx):
+async def get_match(ctx, match_id):
     steam_id = get_steam_id(ctx.message.author)
-    match = stratz.get_match(stratz.get_previous_match_id(steam_id))
-    response = ""
+    match = stratz.get_match(match_id)
 
     if match == []:
         await ctx.reply("Replay is not parsed yet, try again in a minute")
         return
+    
+    return MatchData(match, steam_id)
 
-    for player in match.get("data").get("match").get("players"):    
-        if player.get("steamAccountId") != steam_id:
+def get_previous_match_id_from_author(author):
+    steam_id = get_steam_id(author)
+    return stratz.get_previous_match_id(steam_id)
+
+async def get_previous_match(ctx):
+    return await get_match(ctx, get_previous_match_id_from_author(ctx.message.author))
+
+@bot.command(name="perf", help="Use this command to see how you did last game")
+async def perf(ctx, lang = "eng"):
+    md = await get_previous_match(ctx)
+
+    for player in md.match.get("data").get("match").get("players"):    
+        if player.get("steamAccountId") != md.steam_id:
             continue
 
         imp = player.get("imp")
@@ -47,79 +63,47 @@ async def flink_sjekk(ctx):
         await ctx.reply(response + dota_hero_tips)
 
 @bot.command(name="tips", help="Use this command to apologize for your throws last match")
-async def tips(ctx):
-    steam_id = get_steam_id(ctx.message.author)
-    match = stratz.get_match(stratz.get_previous_match_id(steam_id))
+async def tips(ctx, lang = "eng"):
+    md = await get_previous_match(ctx)
 
-    if match == []:
-        await ctx.send("Replay is not parsed yet, try again in a minute")
-        return
-
-    for player in match.get("data").get("match").get("players"):    
-        if player.get("steamAccountId") != steam_id:
+    for player in md.match.get("data").get("match").get("players"):    
+        if player.get("steamAccountId") != md.steam_id:
             continue
 
         hero = player.get("hero").get("displayName")
-        tips = await openaiclient.prompt_gpt_tips(match, hero)
+        tips = await openaiclient.prompt_gpt_tips(md.match, hero, lang)
 
         await ctx.send(tips)
 
-
 @bot.command(name="sry", help="Use this command to apologize for your throws last match")
-async def apologize(ctx):
-    steam_id = get_steam_id(ctx.message.author)
-    match = stratz.get_match(stratz.get_previous_match_id(steam_id))
+async def apologize(ctx, lang = "eng"):
+    md = await get_previous_match(ctx)
 
-    if match == []:
-        await ctx.send("Replay is not parsed yet, try again in a minute")
-        return
-
-    for player in match.get("data").get("match").get("players"):    
-        if player.get("steamAccountId") != steam_id:
+    for player in md.match.get("data").get("match").get("players"):    
+        if player.get("steamAccountId") != md.steam_id:
             continue
 
         hero = player.get("hero").get("displayName")
-        apology = await openaiclient.prompt_gpt_apology(ctx.message.author.name, hero)
+        apology = await openaiclient.prompt_gpt_apology(md.match, ctx.message.author.name, hero, lang)
 
         await ctx.send(apology)
 
-def get_previous_match_from_author(author):
-    steam_id = get_steam_id(author)
-    match = stratz.get_match(stratz.get_previous_match_id(steam_id))
-
-
 @bot.command(name="anal", help="Analyse the previous game")
-async def analyse(ctx):
-    steam_id = get_steam_id(ctx.message.author)
-    match = stratz.get_match(stratz.get_previous_match_id(steam_id))
-
-    if match == []:
-        await ctx.reply("Replay is not parsed yet, try again in a minute")
-        return
-
-    analysis = await openaiclient.prompt_analyse(match)
+async def analyse(ctx, lang = "eng"):
+    md = await get_previous_match(ctx)
+    analysis = await openaiclient.prompt_analyse(md.match, lang)
     await ctx.reply(analysis)
 
 @bot.command(name="analmatch", help="Analyse a specific game")
-async def analyse(ctx, match_id):
-    match = stratz.get_match(match_id)
-    if match == []:
-        await ctx.reply("Replay is not parsed yet, try again in a minute")
-        return
-
-    analysis = await openaiclient.prompt_analyse(match)
+async def analyse(ctx, match_id, lang = "eng"):
+    md = await get_match(ctx, match_id)
+    analysis = await openaiclient.prompt_analyse(md.match, lang)
     await ctx.reply(analysis)
 
-@bot.command(name="blame", help="Find out who is to blame for your most recent loss")
+@bot.command(name="blame", help="Find out who is to blame for your most recent game")
 async def blame(ctx, lang = "eng"):
-    steam_id = get_steam_id(ctx.message.author)
-    match = stratz.get_match(stratz.get_previous_match_id(steam_id))
-
-    if match == []:
-        await ctx.reply("Replay is not parsed yet, try again in a minute")
-        return
-
-    blame = await openaiclient.prompt_blame(match, lang)
+    md = await get_previous_match(ctx)
+    blame = await openaiclient.prompt_blame(md.match, lang)
     await ctx.reply(blame)
 
 @bot.command(name='reg')
--- openaiclient.py
+++ openaiclient.py
@@ -8,9 +8,9 @@ load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 legend = open("legend.txt")
 
-async def prompt_gpt_apology(sinner, heroname):
+async def prompt_gpt_apology(match, sinner, heroname, lang):
     llm = OpenAI(model_name="text-davinci-003")
-    return llm(f"Write an apologetic short speech about a bad dota 2 match. Write as a player named {sinner}, and include the hero {heroname}.")
+    return llm(f"{default_prompt(match)} write an apologetic short speech about a bad dota 2 match. Write as a player named {sinner}, playing as {heroname}. {lang_preset(lang)}")
 
 async def prompt_gpt_herotip(heroname):
     llm = OpenAI(model_name="text-davinci-003")
@@ -23,17 +23,20 @@ async def prompt_gpt_herotip(heroname):
     input = {'k': 1, 'heroname': heroname}
     return chain.run(input)
 
-async def prompt_analyse(match):
+async def prompt_analyse(match, lang):
     llm = OpenAI(model_name="text-davinci-003")
-    return llm(f"{default_prompt(match)} write a short and witty analysis of the game. You may include some emojis and GIFs from discord.")
+    return llm(f"{default_prompt(match)} write a short and witty analysis of the game. You may include some emojis from discord. {lang_preset(lang)}")
 
 async def prompt_blame(match, lang):
     llm = OpenAI(model_name="text-davinci-003")
-    return llm(f"{default_prompt(match)} write a short witty comment about what player is to blame for the loss. You may include some emojis and GIFs from discord. Write it in the language that has language code {lang}.")
+    return llm(f"{default_prompt(match)} write a short witty comment about what player is to blame for the loss. You may include some emojis from discord. {lang_preset(lang)}")
 
-async def prompt_gpt_tips(match, hero):
+async def prompt_gpt_tips(match, hero, lang):
     llm = OpenAI(model_name="text-davinci-003")
-    return llm(f"{default_prompt(match)} write a short witty comment about how {hero} could improve. Include some general tips. You may include some emojis and GIFs from discord.")
+    return llm(f"{default_prompt(match)} write a short witty comment about how {hero} could improve. Include some general tips. You may include some emojis from discord. {lang_preset(lang)}")
 
 def default_prompt(match):
     return f"Here is data collected about a Dota 2 game from the Stratz API: \n\n {match} and here is some information on how to interpret the data: \n\n{legend.read()} \n\nBased on this data and the interpretation,"
+
+def lang_preset(lang):
+    return f"Write it in the language that has language code {lang}."
